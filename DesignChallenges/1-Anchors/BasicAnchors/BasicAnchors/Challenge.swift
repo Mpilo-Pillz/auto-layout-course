9f29e8bbbd6e23fe04820bdd79a50a82415cee80
mpilopillz <mpilo.dlamini@yahoo.com>
2022-09-20 19:53:27 +0200

    adding gapless playbback with bug on the cross fade

--- DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
+++ DesignChallenges/1-Anchors/BasicAnchors/BasicAnchors/Challenge.swift
@@ -31,12 +31,12 @@ class Challenge: UIViewController {
                 let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
         //
                 let crossfadeLabel = makeBoldLabel(withText: "Crossfade")
-        //        let crossfadeMinLabel = makeSubLabel(withText: "0s")
-        //        let crossfadeMaxLabel = makeSubLabel(withText: "12s")
-        //        let crossfadeProgressView = makeProgressView()
+                let crossfadeMinLabel = makeSubLabel(withText: "0s")
+                let crossfadeMaxLabel = makeSubLabel(withText: "12s")
+                let crossfadeProgressView = makeProgressView()
         //
-        //        let gaplessPlaybackLabel = makeLabel(withText: "Gapless Playback")
-        //        let gaplessPlaybackSwitch = makeSwitch(isOne: true)
+                let gaplessPlaybackLabel = makeLabel(withText: "Gapless Playback")
+                let gaplessPlaybackSwitch = makeSwitch(isOn: true)
         //
         //        let hideSongsLabel = makeLabel(withText: "Hide Unplayable Songs")
         //        let hideSongsSwitch = makeSwitch(isOne: true)
@@ -49,12 +49,12 @@ class Challenge: UIViewController {
                 view.addSubview(offlineSublabel)
         //
                 view.addSubview(crossfadeLabel)
-        //        view.addSubview(crossfadeMinLabel)
-        //        view.addSubview(crossfadeProgressView)
-        //        view.addSubview(crossfadeMaxLabel)
+                view.addSubview(crossfadeMinLabel)
+                view.addSubview(crossfadeProgressView)
+                view.addSubview(crossfadeMaxLabel)
         //
-        //        view.addSubview(gaplessPlaybackLabel)
-        //        view.addSubview(gaplessPlaybackSwitch)
+                view.addSubview(gaplessPlaybackLabel)
+                view.addSubview(gaplessPlaybackSwitch)
         //
         //        view.addSubview(hideSongsLabel)
         //        view.addSubview(hideSongsSwitch)
@@ -69,6 +69,12 @@ class Challenge: UIViewController {
         offlineSublabel.translatesAutoresizingMaskIntoConstraints = false
         
         crossfadeLabel.translatesAutoresizingMaskIntoConstraints = false
+        crossfadeMinLabel.translatesAutoresizingMaskIntoConstraints = false
+        crossfadeMaxLabel.translatesAutoresizingMaskIntoConstraints = false
+        crossfadeProgressView.translatesAutoresizingMaskIntoConstraints = false
+        
+        gaplessPlaybackLabel.translatesAutoresizingMaskIntoConstraints = false
+        gaplessPlaybackSwitch.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             offlineLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
@@ -89,6 +95,32 @@ class Challenge: UIViewController {
         NSLayoutConstraint.activate([
             crossfadeLabel.topAnchor.constraint(equalTo: offlineSublabel.bottomAnchor, constant: 32),
             crossfadeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            crossfadeMinLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: 32),
+            crossfadeMinLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            crossfadeMaxLabel.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: 32),
+            crossfadeMaxLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -32)
+        ])
+        
+        NSLayoutConstraint.activate([
+            crossfadeProgressView.topAnchor.constraint(equalTo: crossfadeLabel.bottomAnchor, constant: 32),
+            crossfadeProgressView.leadingAnchor.constraint(equalTo: crossfadeMinLabel.leadingAnchor, constant: 20),
+            crossfadeProgressView.trailingAnchor.constraint(equalTo: crossfadeMaxLabel.leadingAnchor, constant: -20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            gaplessPlaybackLabel.topAnchor.constraint(equalTo: crossfadeMinLabel.bottomAnchor, constant: 32),
+            gaplessPlaybackLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            gaplessPlaybackSwitch.topAnchor.constraint(equalTo: crossfadeMaxLabel.bottomAnchor, constant: 32),
+            gaplessPlaybackSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
         ])
 
     }
